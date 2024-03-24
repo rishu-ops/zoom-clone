d6be09a019c8a06604024dd33621a746a68b15fa
rishu-ops <rock.jade09@gmail.com>
2024-03-24 19:17:48 +0530

    added singin

--- client/src/assets/index.js
+++ client/src/assets/index.js
@@ -1,4 +1,8 @@
 import bannersinginImage from './registration/banner-singin.png';
+import Homeimage1 from './homepage/Homeimage1.png';
+import Homeimage2 from './homepage/Homeimage2.png';
+import Homeimage3 from './homepage/Homeimage3.png';
+import Homeimage4 from './homepage/Homeimage4.png';
 
 export {
     bannersinginImage
--- client/src/components/singin/Signin.jsx
+++ client/src/components/singin/Signin.jsx
@@ -56,6 +56,7 @@ const Signin = () => {
             </div>
 
         </div>
+
     )
 }
 
