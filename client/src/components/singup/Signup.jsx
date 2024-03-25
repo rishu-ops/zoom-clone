39057afedc7335510c98f7f61559c982accd03ec
Anilkokkul <anilkokkul1997@gmail.com>
2024-03-25 13:35:04 +0530

    Add Yup for form validation and update Signup component with Yup validation
    The diff includes the following changes:
    
    * Added Yup as a dependency in package.json and package-lock.json
    * Created a userDetails state object to store user input values
    * Created a handleSubmit function to handle form submission
    * Added Yup validation for form input fields
    * Updated Signup component to use the new state and validation

--- client/src/components/singup/Signup.jsx
+++ client/src/components/singup/Signup.jsx
@@ -1,39 +1,58 @@
+import { useState } from "react";
 import banner from "../../assets/banner.png";
+import * as yup from "yup";
+
 function Signup() {
+  const userDetails = {
+    fName: "",
+    lName: "",
+    email: "",
+    password: "",
+  };
+  const [user, setUser] = useState(userDetails);
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log("Form Submitted");
+  };
   return (
-    <div className=" md:flex h-screen items-center">
-      <div className=" md:w-1/2 p-16">
-        <img src={banner} alt="banner" />
+    <div className=" lg:flex h-screen items-center lg:mx-16">
+      <div className=" md:w-1/3 hidden lg:block">
+        <img src={banner} alt="banner" className=" xl:ml-36 ml-24" />
       </div>
-      <div className=" md:w-1/2 text-center p-3">
-        <h1 className=" text-3xl md:text-5xl font-semibold my-5">Create Your Account</h1>
+      <div className=" lg:w-2/3 text-center p-3">
+        <h1 className=" text-3x md:text-4xl font-semibold my-8">
+          Create Your Account
+        </h1>
         <p>Enter your full name and password</p>
-        <form className=" md:w-1/2 mx-auto flex flex-col text-center">
+        <form
+          className=" lg:w-1/2 lg:max-w-80 max-w-96 mx-auto flex flex-col"
+          onSubmit={handleSubmit}
+        >
           <input
-            className="border border-orange-900 p-4 my-2 rounded-2xl"
+            className="border border-orange-900 p-3 my-2 rounded-xl"
             type="text"
             placeholder="First name"
           />
           <input
-            className="border border-orange-900 p-4 my-2 rounded-2xl"
+            className="border border-orange-900 p-3 my-2 rounded-xl"
             type="text"
             placeholder="Last name"
           />
           <input
-            className="border border-orange-900 p-4 my-2 rounded-2xl"
+            className="border border-orange-900 p-3 my-2 rounded-xl"
             type="email"
             placeholder="Enter email"
           />
           <input
-            className="border border-orange-900 p-4 my-2 rounded-2xl"
+            className="border border-orange-900 p-3 my-2 rounded-xl"
             type="password"
             placeholder="Password"
           />
           <button
-            className="border bg-slate-200 p-4 my-2 rounded-2xl"
+            className="border bg-slate-200 p-3 my-2 rounded-xl text-gray-600 font-bold"
             type="submit"
           >
-            continue
+            Continue
           </button>
         </form>
       </div>
