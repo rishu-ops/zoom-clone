1078445b8738b5ef82b27791f75096be2f61adce
Anilkokkul <anilkokkul1997@gmail.com>
2024-03-26 20:22:23 +0530

    added server side code with user routes

--- server/app.js
+++ server/app.js
@@ -1,8 +1,17 @@
 const express = require("express");
-
-
+const { db } = require("./db/db.connect");
+require("dotenv").config();
+const userRoutes = require("./routes/user.routes");
 const app = express();
-const PORT = process.env.PORT || 4000
-const server = app.listen(PORT , () => console.log(`server is running on port ${PORT}` ))
 
+db();
+app.use(express.json());
+app.use(userRoutes);
+app.get("/", (req, res) => {
+  res.send(`<h1>Welcome Zoom-clone APIs!</h1>`);
+});
 
+const PORT = process.env.PORT || 4000;
+const server = app.listen(PORT, () =>
+  console.log(`server is running on port ${PORT}`)
+);
